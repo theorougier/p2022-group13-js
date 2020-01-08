f2719028016caf88c517240020b4c933d150b265
theorougier <56915636+theorougier@users.noreply.github.com>
2020-01-08 17:05:04 +0100

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 oxo.screens.loadScreen("home", function() {
-  const btn = document.querySelector(".Menuplay");
+  const btn = document.querySelector(".Menu__play");
   btn.addEventListener("click", function() {
     oxo.screens.loadScreen("instructions", loadInstruction);
   });
