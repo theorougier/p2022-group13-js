633439bc24bb30061d5f498ece59642cc0e3140d
Guilhem Bozzi <guilhem.bozzi@hetic.net>
2020-01-08 14:38:41 +0100

    desapearing cards

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,36 @@
 oxo.screens.loadScreen('home', function() {
 const btn = document.querySelector('.Menu__play')
  btn.addEventListener("click", function(){
-    oxo.screens.loadScreen('game2')
+    oxo.screens.loadScreen('game3', function(){
+
+      var card = document.querySelectorAll('.card__city');
+      
+for(let i=0;i<card.length;i++){
+   card[i].addEventListener('click',function(){
+      card[i].style.opacity=0;
+      })
+
+}
+var card1 = document.querySelectorAll('.card__foret');
+      
+for(let i=0;i<card1.length;i++){
+   card1[i].addEventListener('click',function(){
+      card1[i].style.opacity=0;
+      })
+
+}
+var card2 = document.querySelectorAll('.card__castle');
+      
+for(let i=0;i<card2.length;i++){
+   card2[i].addEventListener('click',function(){
+      card2[i].style.opacity=0;
+      })
+
+}
+
+    })
 
 
  })
 });
-
 
