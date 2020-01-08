89485342570990a3b0e5ed4899d8b0f7fcb5383d
JeanPilous <theo_rougier@hotmail.fr>
2020-01-08 17:53:52 +0100

    Correction card

--- src/js/script.js
+++ src/js/script.js
@@ -10,35 +10,35 @@ const loadInstruction = function() {
   btn2.addEventListener("click", function() {
     oxo.screens.loadScreen("game", loadGame);
     
-     var card = document.querySelectorAll('.card__city');
-      
-for(let i=0;i<card.length;i++){
-   card[i].addEventListener('click',function(){
-      card[i].style.opacity=0;
-      })
-
-}
-var card1 = document.querySelectorAll('.card__foret');
-      
-for(let i=0;i<card1.length;i++){
-
-   card1[i].addEventListener('click',function(){
-      card1[i].style.opacity=0;
-      
-      })
-
-}
-var card2 = document.querySelectorAll('.card__castle');
-      
-for(let i=0;i<card2.length;i++){
-   card2[i].addEventListener('click',function(){
-      card2[i].style.opacity=0;
-      })
-
-}
+   
   });
 };
 
 const loadGame = function() {
+   var card = document.querySelectorAll('.card__city');
+      
+   for(let i=0;i<card.length;i++){
+      card[i].addEventListener('click',function(){
+         card[i].style.opacity=0;
+         })
    
+   }
+   var card1 = document.querySelectorAll('.card__foret');
+         
+   for(let i=0;i<card1.length;i++){
+   
+      card1[i].addEventListener('click',function(){
+         card1[i].style.opacity=0;
+         
+         })
+   
+   }
+   var card2 = document.querySelectorAll('.card__castle');
+         
+   for(let i=0;i<card2.length;i++){
+      card2[i].addEventListener('click',function(){
+         card2[i].style.opacity=0;
+         })
+   
+   }
  };
