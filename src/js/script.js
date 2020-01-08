cb32fd43aa79d23ec13496b1b8fc48f7d051f32b
JeanPilous <theo_rougier@hotmail.fr>
2020-01-08 11:27:49 +0100

    Lvl correction

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 oxo.screens.loadScreen('home', function() {
 const btn = document.querySelector('.Menu__play')
  btn.addEventListener("click", function(){
-    oxo.screens.loadScreen('game')
+    oxo.screens.loadScreen('game3')
 
 
  })
