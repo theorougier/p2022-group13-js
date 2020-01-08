a5f0420ad04eb96f4a9735c538a9751f1f059aa6
tania-abitbol <56915461+tania-abitbol@users.noreply.github.com>
2020-01-08 16:40:38 +0100

    button and instructions page

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,17 @@
-
-oxo.screens.loadScreen('home', function() {
-const btn = document.querySelector('.Menu__play')
- btn.addEventListener("click", function(){
-    oxo.screens.loadScreen('game2')
-
-
- })
+oxo.screens.loadScreen("home", function() {
+  const btn = document.querySelector(".Menu__play");
+  btn.addEventListener("click", function() {
+    oxo.screens.loadScreen("instructions", loadInstruction);
+  });
 });
 
+const loadInstruction = function() {
+  const btn2 = document.querySelector(".instruction__button");
+  btn2.addEventListener("click", function() {
+    oxo.screens.loadScreen("game", loadGame);
+  });
+};
 
+const loadGame = function() {
+   
+ };
