9af148a37b298da3f7a681e2b1b374366c71e9b8
tania-abitbol <56915461+tania-abitbol@users.noreply.github.com>
2020-01-09 14:26:31 +0100

    button pause

--- src/js/script.js
+++ src/js/script.js
@@ -3,113 +3,115 @@ oxo.screens.loadScreen("home", function() {
   btn.addEventListener("click", function() {
     oxo.screens.loadScreen("instructions", loadInstruction);
   });
-
 });
 
 const loadInstruction = function() {
   const btn2 = document.querySelector(".instruction__button");
   btn2.addEventListener("click", function() {
-    oxo.screens.loadScreen("game", loadGame);
-    
-   
-  });
+    oxo.screens.loadScreen("game2", loadGame);
+  });  
 };
 
 const loadGame = function() {
-   
-  var card = document.querySelectorAll('.card__city');
-      
-for(let i=0;i<card.length;i++){
-   card[i].addEventListener('click',function(){
-      card[i].style.opacity=0;
-      })
+  var card = document.querySelectorAll(".card__city");
 
-}
-var card1 = document.querySelectorAll('.card__foret');
-      
-for(let i=0;i<card1.length;i++){
+  for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("click", function() {
+      card[i].style.opacity = 0;
+    });
+  }
+  var card1 = document.querySelectorAll(".card__foret");
 
-   card1[i].addEventListener('click',function(){
-      card1[i].style.opacity=0;
+  for (let i = 0; i < card1.length; i++) {
+    card1[i].addEventListener("click", function() {
+      card1[i].style.opacity = 0;
+    });
+  }
+  var card2 = document.querySelectorAll(".card__castle");
 
-      })
+  for (let i = 0; i < card2.length; i++) {
+    card2[i].addEventListener("click", function() {
+      card2[i].style.opacity = 0;
+    });
+  }
+  var cardtrap = document.querySelectorAll(".trap");
 
-}
-var card2 = document.querySelectorAll('.card__castle');
-      
-for(let i=0;i<card2.length;i++){
-   card2[i].addEventListener('click',function(){
-      card2[i].style.opacity=0;
-      })
+  for (let i = 0; i < cardtrap.length; i++) {
+    cardtrap[i].addEventListener("click", function() {
+      cardtrap[i];
+    });
+  }
+  var cardleft = document.querySelectorAll(".left");
 
-}
-var cardtrap = document.querySelectorAll('.trap');
-      
-for(let i=0;i<cardtrap.length;i++){
-   cardtrap[i].addEventListener('click',function(){
-      cardtrap[i]
-      })
+  for (let i = 0; i < cardleft.length; i++) {
+    cardleft[i].addEventListener("click", function() {
+      cardleft[i];
+    });
+  }
+  var cardright = document.querySelectorAll(".right");
 
-}
-var cardleft = document.querySelectorAll('.left');
-      
-for(let i=0;i<cardleft.length;i++){
-   cardleft[i].addEventListener('click',function(){
-      cardleft[i]
-      })
+  for (let i = 0; i < cardright.length; i++) {
+    cardright[i].addEventListener("click", function() {
+      cardright[i];
+    });
+  }
 
-}
-var cardright = document.querySelectorAll('.right');
-      
-for(let i=0;i<cardright.length;i++){
-   cardright[i].addEventListener('click',function(){
-      cardright[i]
-      })
+  var cardbottomleft = document.querySelectorAll(".bottomleft");
 
-}
+  for (let i = 0; i < cardbottomleft.length; i++) {
+    cardbottomleft[i].addEventListener("click", function() {
+      cardbottomleft[i];
+    });
+  }
 
-var cardbottomleft = document.querySelectorAll('.bottomleft');
-      
-for(let i=0;i<cardbottomleft.length;i++){
-   cardbottomleft[i].addEventListener('click',function(){
-      cardbottomleft[i]
-      })
+  var cardbottomright = document.querySelectorAll(".bottomright");
 
-}
+  for (let i = 0; i < cardbottomright.length; i++) {
+    cardbottomright[i].addEventListener("click", function() {
+      cardbottomright[i];
+    });
+  }
 
-var cardbottomright = document.querySelectorAll('.bottomright');
-      
-for(let i=0;i<cardbottomright.length;i++){
-   cardbottomright[i].addEventListener('click',function(){
-      cardbottomright[i]
-      })
+  var cardline = document.querySelectorAll(".line");
 
-}
+  for (let i = 0; i < cardline.length; i++) {
+    cardline[i].addEventListener("click", function() {
+      cardline[i];
+    });
+  }
 
-var cardline = document.querySelectorAll('.line');
-      
-for(let i=0;i<cardline.length;i++){
-   cardline[i].addEventListener('click',function(){
-      cardline[i]
-      })
+  var cardthreeroad = document.querySelectorAll(".threeroad");
 
-}
+  for (let i = 0; i < cardthreeroad.length; i++) {
+    cardthreeroad[i].addEventListener("click", function() {
+      cardthreeroad[i];
+    });
+  }
 
-var cardthreeroad = document.querySelectorAll('.threeroad');
-      
-for(let i=0;i<cardthreeroad.length;i++){
-   cardthreeroad[i].addEventListener('click',function(){
-      cardthreeroad[i]
-      })
+  var cardthreeroadleft = document.querySelectorAll(".threeroadleft");
 
-}
+  for (let i = 0; i < cardthreeroadleft.length; i++) {
+    cardthreeroadleft[i].addEventListener("click", function() {
+      cardthreeroadleft[i];
+    });
+  }
 
-var cardthreeroadleft = document.querySelectorAll('.threeroadleft');
-      
-for(let i=0;i<cardthreeroadleft.length;i++){
-   cardthreeroadleft[i].addEventListener('click',function(){
-      cardthreeroadleft[i]
-      })
+  const btn3 = document.querySelector(".return");
+  btn3.addEventListener("click", function() {
+    oxo.screens.loadScreen("home", loadReturn);
+  });
 
-}
- };
+  const loadReturn = function() {};
+  let pause2 = document.querySelector(".depause");
+  let pause = document.querySelector(".niv");
+  let boutonpause = document.querySelector(".pause");
+  boutonpause.addEventListener("click", function() {
+    pause.classList.toggle("hidden");
+    pause2.classList.toggle("hidden");
+  });
+
+  pause2.addEventListener("click", function() {
+    pause.classList.toggle("hidden");
+    pause2.classList.toggle("hidden");
+  });
+};
